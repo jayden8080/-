fee74a9f30354697eab4c8b39c138e80312bcd94
jayden8080 <gimjuhan416@gmail.com>
2020-08-14 19:13:19 +0900

    add clear command

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.token;
+const token = 'NzMyNjAyMjUwNzUyNjg4MjA4.Xw2_Aw.O9P2nYi8Jge3XYokQbQu2VOQWAE';
 
 client.on('ready', () => {
   console.log('켰다.');
 });
 
 client.on('message', (message) => {
-  if(message.content === 'ping') {
-    message.reply('pong');
+  if(message.content === '안녕하세요') {
+    message.reply('어서오세요');
   }
 });
 
